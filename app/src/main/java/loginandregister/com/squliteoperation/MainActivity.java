11fa0e1680f6bcea4c026860a30ffd1a256c6073
Varun Ladha <varunladh789@gmail.com>
2020-05-23 13:42:22 +0530

    Commited

---
+++ app/src/main/java/loginandregister/com/squliteoperation/MainActivity.java
@@ -0,0 +1,20 @@
+package loginandregister.com.squliteoperation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText name,password;
+    Button view,add;
+    myDBAdapter Helper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name=(EditText)findViewById(R.id.edt_username);
+        password=(EditText)findViewById(R.id.)
+    }
+}
